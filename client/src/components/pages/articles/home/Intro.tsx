75a7533162d7516635972ed23bd2ea98158ff7b8
Sanaz-Shahed79 <100836125+Sanaz-Shahed79@users.noreply.github.com>
2022-09-01 10:59:39 +0200

    Delete Intro.tsx

--- client/src/components/pages/articles/home/Intro.tsx
+++
@@ -1,9 +0,0 @@
-function intro() {
-    return (
-        <section id="intro">
-            <h1>SWEDEN NEWS</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dolorum quia inventore iusto! Reprehenderit ad accusantium dicta blanditiis nulla nisi.</p>
-        </section>
-    )
-}
-export default intro;
