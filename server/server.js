9a874b703e640200c2c5cd02e2abd23195b36e78
SanazShahed <Shahed.sanaz12@gmail.com>
2022-09-20 21:04:41 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,52 +1,70 @@
 import express from "express";
 import fetch from 'node-fetch'
 import cors from 'cors';
+import fs from 'fs';
+const articlesData = './articles.json'
 
 
 
 const app = express()
-const port = 5013
+const port = 3000
 const apiKey = '5b069832543f4054a3c1a5fe2a205243'
-
-
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
 
-//  get(route,callback function);
-app.get("/api", (req, res) => {
-    res.send("Articles")
-})
 
 // GET all articles
 app.get("/api/articles", async (req, res) => {
-    let response = await fetch(`https://630bd252ed18e8251659652a.mockapi.io/home`)
-    let articles = await response.json();
-    res.json(articles)
+    fs.readFile(articlesData, 'utf8', (err, data) => {
+        res.json(JSON.parse(data)); 
+    });
 })
 
-// GET an articles
+app.get("/api/external", async (req, res) => {
+    let response = await fetch(`https://630bd252ed18e8251659652a.mockapi.io/home`)
+    let result = await response.json();
+    fs.writeFile(articlesData, JSON.stringify(result), (err) => {
+        res.json("Done");
+    })
+})
+
+// GET an article
 app.get("/api/articles/:id", async (req, res) => {
-    let response = await fetch(`https://630bd252ed18e8251659652a.mockapi.io/home/${req.params.id}`)
-    let articles = await response.json();
-    res.json(articles)
+    const id = req.params.id
+    fs.readFile(articlesData, 'utf8', (err, data) => {
+        const article = JSON.parse(data).find(article => article.id == id)
+        res.json(article); 
+    });
 })
 
 // Create an article
 app.post("/api/articles/add", async (req, res) => {
-    
-    let sendArticle = await fetch('https://630bd252ed18e8251659652a.mockapi.io/home', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          title: req.body.title,
-          description: req.body.description,
+    let newArticle = {
+        id : Math.round(Math.random(111111,666666) * 1000000),
+        title: req.body.title,
+        image : 'https://picsum.photos/600/400',
+        text: req.body.description,
+        created_at : new Date()
+    }
+
+    fs.readFile(articlesData, 'utf8', (err, data) => {
+        let articles = JSON.parse(data);
+        articles.push(newArticle);
+        fs.writeFile(articlesData, JSON.stringify(articles), (err) => {
+            res.json("Ny artikel sparad");
         })
-      })
-      let newArticle = await sendArticle.json();
-    res.json(newArticle)
+    });
+   
+
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 })
+
+/*async function getArticles(){
+    fs.readFile(articlesData, 'utf8', (err, data) => {
+        res.json(JSON.parse(data)); 
+    });
+}*/
